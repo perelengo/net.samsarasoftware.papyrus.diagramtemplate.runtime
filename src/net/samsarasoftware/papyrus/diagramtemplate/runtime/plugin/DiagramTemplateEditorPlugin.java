378d81a750aaf9194ba4a0a2984d336d1d4bc1f2
Pere Joseph Rodríguez <perelengo@hotmail.com>
2022-09-08 17:34:48 +0200

    Remove license from generator code and replace by its real license

--- src/net/samsarasoftware/papyrus/diagramtemplate/runtime/plugin/DiagramTemplateEditorPlugin.java
+++ src/net/samsarasoftware/papyrus/diagramtemplate/runtime/plugin/DiagramTemplateEditorPlugin.java
@@ -1,16 +1,3 @@
-/*****************************************************************************
- * Copyright (c) 2010 CEA LIST.
- *
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *  CEA LIST - Initial API and implementation
- *
- *****************************************************************************/
 package net.samsarasoftware.papyrus.diagramtemplate.runtime.plugin;
 
 
@@ -39,39 +26,23 @@ import org.eclipse.emf.common.ui.EclipseUIPlugin;
 import org.eclipse.emf.common.util.ResourceLocator;
 
 
-/**
- * This is the central singleton for the DiagramTemplate editor plugin.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
- *
- * @generated
- */
 public final class DiagramTemplateEditorPlugin extends EMFPlugin {
 
 	/**
 	 * Keep track of the singleton.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
 	 *
-	 * @generated
 	 */
 	public static final DiagramTemplateEditorPlugin INSTANCE = new DiagramTemplateEditorPlugin();
 
 	/**
 	 * Keep track of the singleton.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
 	 *
-	 * @generated
 	 */
 	private static Implementation plugin;
 
 	/**
 	 * Create the instance.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
 	 *
-	 * @generated
 	 */
 	public DiagramTemplateEditorPlugin() {
 		super(new ResourceLocator[] {});
@@ -79,11 +50,8 @@ public final class DiagramTemplateEditorPlugin extends EMFPlugin {
 
 	/**
 	 * Returns the singleton instance of the Eclipse plugin.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 *
 	 * @return the singleton instance.
-	 * @generated
+	 *
 	 */
 	@Override
 	public ResourceLocator getPluginResourceLocator() {
@@ -92,11 +60,8 @@ public final class DiagramTemplateEditorPlugin extends EMFPlugin {
 
 	/**
 	 * Returns the singleton instance of the Eclipse plugin.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 *
 	 * @return the singleton instance.
-	 * @generated
+	 *
 	 */
 	public static Implementation getPlugin() {
 		return plugin;
@@ -104,19 +69,13 @@ public final class DiagramTemplateEditorPlugin extends EMFPlugin {
 
 	/**
 	 * The actual implementation of the Eclipse <b>Plugin</b>.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
 	 *
-	 * @generated
 	 */
 	public static class Implementation extends EclipseUIPlugin {
 
 		/**
 		 * Creates an instance.
-		 * <!-- begin-user-doc -->
-		 * <!-- end-user-doc -->
 		 *
-		 * @generated
 		 */
 		public Implementation() {
 			super();
