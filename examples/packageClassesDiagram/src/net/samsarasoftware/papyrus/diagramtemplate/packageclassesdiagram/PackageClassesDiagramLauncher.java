6cc5ead501753ffa466b41beafdb92c9f09e23d5
Pere Joseph Rodríguez <perelengo@hotmail.com>
2022-09-23 11:23:18 +0200

    Fix script loading when deployed as plugin

--- examples/packageClassesDiagram/src/net/samsarasoftware/papyrus/diagramtemplate/packageclassesdiagram/PackageClassesDiagramLauncher.java
+++ examples/packageClassesDiagram/src/net/samsarasoftware/papyrus/diagramtemplate/packageclassesdiagram/PackageClassesDiagramLauncher.java
@@ -1,10 +1,11 @@
 package net.samsarasoftware.papyrus.diagramtemplate.packageclassesdiagram;
 
-import java.net.URL;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.EnumSet;
 
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
+import org.apache.commons.io.IOUtils;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.elk.core.LayoutConfigurator;
 import org.eclipse.elk.core.math.ElkPadding;
@@ -14,10 +15,10 @@ import org.eclipse.elk.core.options.EdgeRouting;
 import org.eclipse.elk.core.options.SizeConstraint;
 import org.eclipse.elk.core.options.SizeOptions;
 import org.eclipse.elk.core.service.DiagramLayoutEngine;
+import org.eclipse.elk.core.service.DiagramLayoutEngine.Parameters;
 import org.eclipse.elk.core.service.ILayoutListener;
 import org.eclipse.elk.core.service.LayoutConnectorsService;
 import org.eclipse.elk.core.service.LayoutMapping;
-import org.eclipse.elk.core.service.DiagramLayoutEngine.Parameters;
 import org.eclipse.elk.core.util.IElkProgressMonitor;
 import org.eclipse.elk.graph.ElkGraphElement;
 import org.eclipse.elk.graph.ElkNode;
@@ -39,11 +40,36 @@ import net.samsarasoftware.papyrus.diagramtemplate.runtime.DiagramTemplateLaunch
 public class PackageClassesDiagramLauncher extends DiagramTemplateLauncher {
 
 	protected  URI getTemplateURI() throws Exception {
-		Bundle bundle = Platform.getBundle("net.samsarasoftware.papyrus.diagramtemplate.packageclassesdiagram");
-		URL url = FileLocator.find(bundle, new Path("resources/script.di"), null);
-		url = FileLocator.toFileURL(url);
-		URI uri= URI.createURI(url.toString());
-		return uri;	
+		Bundle bundle = Platform.getBundle(this.getClass().getPackageName());
+		InputStream diIs=null;
+		FileOutputStream diOs=null;
+		InputStream notIs=null;
+		FileOutputStream  notOs=null;
+		InputStream umlIs=null;
+		FileOutputStream  umlOs=null;
+		
+		diIs=bundle.getEntry("resources/script.di").openStream();
+		notIs=bundle.getEntry("resources/script.notation").openStream();
+		umlIs=bundle.getEntry("resources/script.uml").openStream();
+
+		File diFile=new File(System.getProperty("java.io.tmpdir")+((System.getProperty("java.io.tmpdir").endsWith(File.separator))?"":File.separator)+"script.di");
+		File notFile=new File(diFile.getAbsolutePath().substring(0,diFile.getAbsolutePath().length()-2)+"notation");
+		File umlFile=new File(diFile.getAbsolutePath().substring(0,diFile.getAbsolutePath().length()-2)+"uml");
+
+		
+		diOs=new FileOutputStream(diFile);
+		notOs=new FileOutputStream(notFile);
+		umlOs=new FileOutputStream(umlFile);
+		
+		diFile.deleteOnExit();
+		notFile.deleteOnExit();
+		umlFile.deleteOnExit();
+		
+		IOUtils.copy(diIs, diOs);
+		IOUtils.copy(notIs, notOs);
+		IOUtils.copy(umlIs, umlOs);
+
+		return URI.createFileURI(diFile.getAbsolutePath());	
 	}
 	
 	/**
